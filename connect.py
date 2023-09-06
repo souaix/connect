3f5e779f4798132adae478906d78d3b2a1cb8d34
souaix <souaix@gmail.com>
2023-09-06 10:02:54 +0800

    LINUX CONNECTOR

--- connect.py
+++ connect.py
@@ -5,25 +5,27 @@ import cx_Oracle
 
 def connect(srv, db):
 
-    if srv == 'MES':
+	if srv == 'MES':
 
-        engine = create_engine(
-            'mssql+pyodbc://CIMADMIN:theil4893701@10.21.150.108/'+db+'?charset=utf8mb4&driver=SQL+Server+Native+Client+11.0')
-        con = engine.connect()  # 建立連線
+		engine = create_engine(
+		    'mssql+pyodbc://CIMADMIN:theil4893701@10.21.150.108/'+db+'?charset=utf8mb4&driver=ODBC Driver 18 for SQL Server&TrustServerCertificate=yes')
+		#con = engine.connect()  # 建立連線
 
-    elif srv == 'CIM':
+	elif srv == 'CIM':
 
-        engine = create_engine(
-            'mysql+pymysql://thiler:thil1234@10.21.40.126/'+db+'?charset=utf8mb4')
-        con = engine.connect()  # 建立連線
+		engine = create_engine(
+		    'mysql+pymysql://thiler:thil1234@10.21.40.126/'+db+'?charset=utf8mb4')
+		#con = engine.connect()  # 建立連線
 
-    elif srv == 'SAP':
+	elif srv == 'SAP':
 
-        # engine = create_engine(
-        #     "oracle+cx_oracle://thmes:qxmk2965@10.21.1.32:1521/?service_name=LTINTER", max_identifier_length=30)
-        # con = engine.connect()  # 建立連線
+		engine = cx_Oracle.connect("thmes","qxmk2965","10.21.1.32:1521/LTINTER",encoding="UTF-8")
 
-        con = cx_Oracle.connect('thmes/qxmk2965@10.21.1.32:1521/LTINTER',
-                                encoding='UTF-8', nencoding='UTF-8')
-        engine = ''
-    return engine, con
+
+#		con = create_engine(
+#		"oracle+cx_oracle://thmes:qxmk2965@10.21.1.32:1521/?service_name=LTINTER")
+	 #con = engine.connect()  # 建立連線
+
+	#con = cx_Oracle.connect('thmes/qxmk2965@10.21.1.32:1521/LTINTER',
+	#                        encoding='UTF-8', nencoding='UTF-8')  
+	return engine
