cd6e7591f56cf238cfb0a8b7d80d61f3c85c9d44
souaix <souaix@gmail.com>
2023-09-23 09:07:49 +0800

    Add Cim_ubuntu connector

--- connect.py
+++ connect.py
@@ -2,6 +2,7 @@ from sqlalchemy import types, create_engine
 import pyodbc
 import cx_Oracle
 from sqlalchemy import text
+from sqlalchemy.pool import NullPool
 
 def connect(srv, db):
 
@@ -17,9 +18,24 @@ def connect(srv, db):
 		    'mysql+pymysql://thiler:thil1234@10.21.40.126/'+db+'?charset=utf8mb4',pool_recycle=5)
 		#con = engine.connect()  # 建立連線
 
+	elif srv == 'CIM_ubuntu':
+
+			engine = create_engine(
+				'mysql+pymysql://cim:theil4893701@10.21.98.21/'+db+'?charset=utf8mb4',pool_recycle=5)
+			#con = engine.connect()  # 建立連線
+
+
+
 	elif srv == 'SAP':
 
-		engine = cx_Oracle.connect("thmes","qxmk2965","10.21.1.32:1521/LTINTER",encoding="UTF-8")
+		if db =='SAP_Test':
+
+			engine = cx_Oracle.connect("thmes","qxmk2965","10.21.1.32:1521/LTINTER",encoding="UTF-8")
+
+		elif db =='SAP_PRD':
+
+                       engine = cx_Oracle.connect("thmes","qxmk2965","10.21.1.52:1521/LTINTER",encoding="UTF-8")
+
 
 
 #		con = create_engine(
